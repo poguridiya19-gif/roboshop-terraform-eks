cbf3574e6e17f91e40b768ea016ce1d3a2c976f1
Poguri Diya <diya>
2026-04-06 11:19:26 +0530

    clean commit

---
+++ 10-sg/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        # "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        # "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "ingress_alb" ,
+        # backened load balancer
+        # "backend_alb" ,
+
+        "open_vpn",
+        "eks_control_plane",
+        "eks_node"
+
+    ]
+}
