e706330d09ab279cd5dd57721237674afb828f1f
Poguri Diya <diya>
2026-04-06 13:00:36 +0530

    k8

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "poguri.fun"
+}
+
+variable "zone_id"{
+    default = "Z09362922S2ZXJ8DP3JCV"
+}
